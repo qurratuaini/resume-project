c9c5a79e311cc845f63dafbe2e4d529c75f2b15d
Sage Elliott <wildtroll@gmail.com>
2015-09-21 15:20:48 -0700

    feat: add name and role to resume:
    - create variable with full name
    - create variable with role
    - add formattedName to HTMLheaderName
    - add formattedRole to HTMLheaderRole

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+var name = "Sage Elliott";
+
+var role = "Web Developer";
+
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+$("#header").append(formattedName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedRole);
