0e53dc4da95eb2d08ace7dda90548af8f5a7fedd
Sage Elliott <wildtroll@gmail.com>
2015-09-21 17:20:16 -0700

    add if statement to display three skills if skills list is greater than 0

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -23,7 +23,19 @@ $("#header").prepend(formattedRole);
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 $("#header").prepend(formattedName);
 
+if(bio.skills.length > 0) {
 
+    $("#header").append(HTMLskillsStart);
+
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
+    $("#skills").append(formattedSkill);
+
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[1]);
+    $("#skills").append(formattedSkill);
+
+    formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
+    $("#skills").append(formattedSkill);
+};
 
 
 // Work History
